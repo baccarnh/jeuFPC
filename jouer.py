55ecacf4f3e8f8245e706ede9fff0d0ebfbeb15f
nour <baccarnh@gmail.com>
2019-11-06 14:17:15 +0100

    CHOIXPC function defined and package of 3 functions tested

--- jouer.py
+++ jouer.py
@@ -7,17 +7,22 @@ def ENTRE():
         print("nom du joueur=", nom.upper())
 
 def CHOIX():
-    ch=input("entrer votre choix parmi feuille/pièrre/ciseaux")
-    ch=ch.upper() #accept all type of letters
+    ch=input("entrer votre choix parmi feuille/pierre/ciseaux")
+    ch=ch.upper() ##accept all type of letters
     FPC=['FEUILLE', 'PIERRE', 'CISEAUX']
-    while ch not in FPC: # verify that the choice is OK
+    while ch not in FPC: ##verify that the choice is OK
         print("erreur choix non valide")
         ch=input("rentrer votre choix de nouveau")
     else:
-        print("le choix du joueur est {}".format(ch))
+        print("le choix du joueur est", ch)
 
-
+def CHOIXPC():
+    import random
+    FPC = ['FEUILLE', 'PIERRE', 'CISEAUX']
+    rch=random.choice(FPC)
+    print('le choix de lordinateur est', rch)
 
 
 ENTRE()
 CHOIX()
+CHOIXPC()
