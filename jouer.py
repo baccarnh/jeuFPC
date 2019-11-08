856a027d9a4a0d1a747ea906e90d6322621dab40
nour <baccarnh@gmail.com>
2019-11-08 12:12:42 +0100

    better message

--- jouer.py
+++ jouer.py
@@ -4,36 +4,35 @@ def ENTRE():
         print("erreur renter votre nom de minimum 2 lettres")
         nom = input("entrer votre nom")
     else:
-        print("nom du joueur=", nom.upper())
+        print("nom du joueur=".center(50), nom.upper().center(50))
 
 def CHOIX():
-    ch = input("entrer votre choix parmi feuille/pierre/ciseaux").upper()
-    # ch=ch.upper() ##accept all type of letters
+    ch = input("entrer votre choix parmi feuille/pierre/ciseaux").upper() ##accept all type of letters
     FPC = ['FEUILLE', 'PIERRE', 'CISEAUX']
     while ch not in FPC:  ##verify that the choice is OK
         print("erreur choix non valide")
-        ch = input("entrer votre choix parmi feuille/pierre/ciseaux").upper()
+        ch = input("entrer votre choix parmi feuille/pierre/ciseaux").upper() ##ask the choice again
     if ch in FPC:
-        print("le choix du joueur est", ch)
+        print("le choix du joueur est".center(40), ch.center(40))
     return ch
 
 def CHOIXPC():
     import random
     FPC = ['FEUILLE', 'PIERRE', 'CISEAUX']
     rch=random.choice(FPC)#the computer choice
-    print('le choix de lordinateur est', rch)#present the computer choice to the player
+    print("le choix de l'ordinateur est".center(40), rch.center(40))#present the computer choice to the player
     return rch
 
 
 def ATTRIBUE():
     scor_pl= 0
     scor_comp= 0
-    while scor_pl!=3 and scor_comp!=3:
+    while scor_pl!=3 and scor_comp!=3:##repeat the game until one of the computer or the player has 3 point
         player_c=CHOIX()
         computer_c=CHOIXPC()
-        player_win=[('PIERRE','CISEAUX'), ('FEUILLE','PIERRE'), ('CISEAUX', 'FEUILLE')]
-        computer_win=[('PIERRE','FEUILLE'), ('FEUILLE','CISEAUX'), ('CISEAUX', 'PIERRE') ]
-        if player_c==computer_c:
+        player_win=[('PIERRE','CISEAUX'), ('FEUILLE','PIERRE'), ('CISEAUX', 'FEUILLE')] ##a list that the player win
+        computer_win=[('PIERRE','FEUILLE'), ('FEUILLE','CISEAUX'), ('CISEAUX', 'PIERRE')] ##a list yhet the computer win
+        if player_c==computer_c:#if have the same choice 0point added for both
             scor_pl+=0
             scor_comp+=0
             print("vous avez fait le meme choix que la machine vos scores")
@@ -43,18 +42,21 @@ def ATTRIBUE():
         elif (player_c, computer_c) in computer_win :
             scor_pl+=0
             scor_comp+=1
-        print("le score du joueur est",scor_pl, "le score de la machine est", scor_comp)
+        print("**************************************************************************")
+        print("le score du joueur est".center(40),scor_pl)
+        print("le score de la machine est".center(40), scor_comp)
+        print("**************************************************************************")
     if scor_pl==3:
-        print('la partie est finie, vous avez gagné')
+        print("la partie est finie, œœœœœœœœœœœœœœœœœœœœœœ vous avez gagné œœœœœœœœœœœœœœœœœœœœœœœ")
     elif scor_comp==3:
-        print('la partie est finie, vous avez perdu')
+        print("la partie est finie, :vous avez perdu")
     return (scor_pl, scor_comp)
 
 
 def PLAYAGAIN():
-    pl = input("voulez vous jouer de nouveau? taper oui ou non").upper()
+    pl = input("voulez vous jouer de nouveau? taper oui ou non").upper()#accept oui/non
     tag=["OUI", "NON"]
-    while pl not in tag:
+    while pl not in tag:#if input different =error
         print("erreur de saisie taper oui ou non")
         pl = input("entrer votre réponce de nouveau").upper()
     if pl=="NON":
